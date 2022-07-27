dbeaf75ffd46b6899eab05ea0e6f262118dcbb41
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-07-27 17:17:28 +0900

    販売手数料・利益をJavaScriptで実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,20 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
   # ジャンルの選択が「--」の時は保存不可
   with_options numericality: { other_than: 0 } do
     validates :category_id
