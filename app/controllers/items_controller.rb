aeb51f159b746bdab112676bef701321caae44d2
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-07-31 11:08:12 +0900

    テストコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
   
   #def index
     #@items = Item.all
@@ -37,8 +37,8 @@ class ItemsController < ApplicationController
     #end
   #end
 
-  def show
-  end
+  #def show
+  #end
 
   private
 
