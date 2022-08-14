2be73efd1090f9cdd5f2668a8a265618474ecc42
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-08-14 09:05:57 +0900

    購入後トップページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,21 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-
+  
   def index
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
   end
 
+
   def create
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
       pay_item
       @order_form.save
       redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
@@ -24,7 +26,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,        # 商品の値段
       card: order_params[:token], # カードトークン
