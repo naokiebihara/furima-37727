f95b2a35c6b1837f30ac8873650404a4446a627d
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-08-15 09:54:28 +0900

    sold out表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one    :order
-
+  
   # アクティブハッシュとのアソシエーション
   belongs_to :category
   belongs_to :prefecture
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-              
+    validates :user_id          
     validates :image
     validates :name
     validates :description
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :shipping_date_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   # ジャンルの選択が「--」の時は保存不可
