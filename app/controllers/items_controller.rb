0b8cdfdb5d86a437e1581f6387e2f67ecdbf3e41
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-08-16 16:12:23 +0900

    商品一覧表示機能 ダミー画像修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
-    @items = Item.all.order(created_at: :desc)
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
